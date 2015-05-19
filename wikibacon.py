303b50f12e0d755e46f60cb5c70a0661197c7d74
unknown <03mcginley33@DEBN-L339.msanet.com>
2015-05-19 16:21:57 +0200

    I can read one website, and have an object, but can't automatically generate them yet

---
+++ wikibacon.py
@@ -0,0 +1,42 @@
+__author__ = '03mcginley33'
+
+
+import requests
+from bs4 import BeautifulSoup
+
+#imports all the stuff from the url
+url = "http://en.wikipedia.org/wiki/Armadillo"
+r = requests.get(url)
+soup = BeautifulSoup(r.text)
+
+#based on the known location of the title, saves that as the name for the wikipage object
+titleLine = soup.find_all('h1', {'class':'firstHeading'})
+for t in titleLine:
+    #this gets fed into the wikipage object
+    wikiName = t.contents[0]
+
+#find everything in the lowest level div class that contains the links we want, makes a list
+links = soup.find_all('div', {'class':'mw-content-ltr'})
+
+#run through that list and pull out all the anchor things that will contain a link
+for link in links:
+    thatLink = link.find_all('a', href = True)
+
+#for each one of those, print the text of the link that contains the beginning of the interlink url
+for x in thatLink:
+    if str(x.get('href')).startswith("/wiki/"):
+        #this also gets fed into the wikipage object
+        wikiLinks = (x['href'])
+
+
+#set up the class of wikipedia pages, which will have a name and links
+class WikiPage (object):
+    def __init__(self, name, linksList):
+        self.name = name
+        self.linksList = linksList
+
+#make one instance of the WikiPage object with the name and links from above
+a = WikiPage(wikiName, wikiLinks)
+print(a.name)
+
+
