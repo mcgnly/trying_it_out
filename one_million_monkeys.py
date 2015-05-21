c659403c27f614b70175ba06ab3d335bae4aca25
James Munns <james.munns@gmail.com>
2015-05-21 11:34:46 +0200

    Add code review comments

--- one_million_monkeys.py
+++ one_million_monkeys.py
@@ -1,20 +1,28 @@
 __author__ = '03mcginley33'
 
 # not sure why I have to import string really...
+# AJM - you have to import string to get access to string.xxx that you use below.
 import string
 import random
+
+# Its better practice to always use variables, rather than "Magic Numbers", especially
+#  if you use it multiple times.
+MAX_TRIES = 1000000
 
 #this is the text to match
 personText = input("What should the monkeys type for you today? ")
 #this generates an empty list of the same length as the inputted text, because strings are immutable
-monkeyText = []*len(personText)
+#AJM - it is not necessary to initalize the size of the array, and what you were doing before actually is the same as I have here. Lists grow as necessary, you just cant access more than the current length.
+monkeyText = []
 
 #generates a random character of upper, lower, punctuation, and spaces
 def randomChar():
     return random.choice(string.ascii_letters + string.punctuation + ' ')
 
 #populates the empty list with random characters
-for i in range(len(personText)):
+# Almost any time you see "range(len(x))", something is wrong. Just do for x in y: instead.
+# Additionally, using '_' as the variable name here is a hint to other programmers you dont care about each item, you are just iterating that many times.
+for _ in personText:
     monkeyText.append(randomChar())
 
 print(monkeyText)
@@ -22,7 +30,7 @@ print(monkeyText)
 count = 0
 
 #now to start randomizing the individual positions until they match, upper bound at a million
-while personText != monkeyText and count < 1000000:
+while personText != monkeyText and count < MAX_TRIES:
     #turn it into a list so we can work with it
     newMonkey = list(monkeyText)
 
@@ -39,7 +47,7 @@ while personText != monkeyText and count < 1000000:
 
 
 #print results
-if count < 1000000:
-    print("Your text took %s  monkeys to write." % count)
+if count < MAX_TRIES:
+    print("Your text took %s monkeys to write." % count)
 else:
     print("not even one million monkeys could finish that in time.")
