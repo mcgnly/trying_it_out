ca9b735df46777f715c91e9086eca58c5ecfc34b
unknown <03mcginley33@DEBN-L339.msanet.com>
2015-06-10 15:11:44 +0200

    change to main style rather than script style

--- one_million_monkeys.py
+++ one_million_monkeys.py
@@ -4,46 +4,50 @@ __author__ = '03mcginley33'
 import string
 import random
 
-#this is the text to match
-personText = input("What should the monkeys type for you today? ")
-#this generates an empty list of arbitrary length, because strings are immutable
-#Lists grow as necessary, you just cant access more than the current length
-monkeyText = []
-#get rid of magic numbers
-MAX_TRIES = 1000000
+def OneMillionMonkeys():
+        #this is the text to match
+    personText = input("What should the monkeys type for you today? ")
+    #this generates an empty list of arbitrary length, because strings are immutable
+    #Lists grow as necessary, you just cant access more than the current length
+    monkeyText = []
+    #get rid of magic numbers
+    MAX_TRIES = 1000000
 
-#generates a random character of upper, lower, punctuation, and spaces
-def randomChar():
-    return random.choice(string.ascii_letters + string.punctuation + ' ')
+    #generates a random character of upper, lower, punctuation, and spaces
+    def randomChar():
+        return random.choice(string.ascii_letters + string.punctuation + ' ')
 
-#populates the empty list with random characters, _ shows we don't care about which we are on, just do it to all
-#this is the same as for i in range(length) cause it just does it that many times anyway, not TO personText
-for _ in personText:
-    monkeyText.append(randomChar())
+    #populates the empty list with random characters, _ shows we don't care about which we are on, just do it to all
+    #this is the same as for i in range(length) cause it just does it that many times anyway, not TO personText
+    for _ in personText:
+        monkeyText.append(randomChar())
 
-print(monkeyText)
-#initialize a count to see how many iterations it takes
-count = 0
-
-#now to start randomizing the individual positions until they match, upper bound at a million
-while personText != monkeyText and count < MAX_TRIES:
-    #turn it into a list so we can work with it
-    newMonkey = list(monkeyText)
-
-    #step through each character if it doesn't match
-    for j, c in enumerate(monkeyText):
-        if personText[j] != c:
-            newMonkey[j] = randomChar()
-            monkeyText = ''.join(newMonkey)
-
-    #increment counter to tell how many tries it took
-    count += 1
-    #print out iteration until it matches original text
     print(monkeyText)
+    #initialize a count to see how many iterations it takes
+    count = 0
+
+    #now to start randomizing the individual positions until they match, upper bound at a million
+    while personText != monkeyText and count < MAX_TRIES:
+        #turn it into a list so we can work with it
+        newMonkey = list(monkeyText)
+
+        #step through each character if it doesn't match
+        for j, c in enumerate(monkeyText):
+            if personText[j] != c:
+                newMonkey[j] = randomChar()
+                monkeyText = ''.join(newMonkey)
+
+        #increment counter to tell how many tries it took
+        count += 1
+        #print out iteration until it matches original text
+        print(monkeyText)
 
 
-#print results
-if count < MAX_TRIES:
-    print("Your text took %s  monkeys to write." % count)
-else:
-    print("not even one million monkeys could finish that in time.")
+    #print results
+    if count < MAX_TRIES:
+        print("Your text took %s  monkeys to write." % count)
+    else:
+        print("not even one million monkeys could finish that in time.")
+
+if __name__ == "__main__":
+    OneMillionMonkeys()
