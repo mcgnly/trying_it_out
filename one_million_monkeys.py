ad9dfa96d6aa91af875e3752474b77072b33573b
unknown <03mcginley33@DEBN-L339.msanet.com>
2015-06-10 13:48:45 +0200

    get rid of magic numbers, change the way lists are iterated through

--- one_million_monkeys.py
+++ one_million_monkeys.py
@@ -6,15 +6,19 @@ import random
 
 #this is the text to match
 personText = input("What should the monkeys type for you today? ")
-#this generates an empty list of the same length as the inputted text, because strings are immutable
-monkeyText = []*len(personText)
+#this generates an empty list of arbitrary length, because strings are immutable
+#Lists grow as necessary, you just cant access more than the current length
+monkeyText = []
+#get rid of magic numbers
+MAX_TRIES = 1000000
 
 #generates a random character of upper, lower, punctuation, and spaces
 def randomChar():
     return random.choice(string.ascii_letters + string.punctuation + ' ')
 
-#populates the empty list with random characters
-for i in range(len(personText)):
+#populates the empty list with random characters, _ shows we don't care about which we are on, just do it to all
+#this is the same as for i in range(length) cause it just does it that many times anyway, not TO personText
+for _ in personText:
     monkeyText.append(randomChar())
 
 print(monkeyText)
@@ -22,7 +26,7 @@ print(monkeyText)
 count = 0
 
 #now to start randomizing the individual positions until they match, upper bound at a million
-while personText != monkeyText and count < 1000000:
+while personText != monkeyText and count < MAX_TRIES:
     #turn it into a list so we can work with it
     newMonkey = list(monkeyText)
 
@@ -39,7 +43,7 @@ while personText != monkeyText and count < 1000000:
 
 
 #print results
-if count < 1000000:
+if count < MAX_TRIES:
     print("Your text took %s  monkeys to write." % count)
 else:
     print("not even one million monkeys could finish that in time.")
